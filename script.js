f35b5891f5d959214560d7dce5b1b0f30a218451
Stijn <stijn@ardusit.nl>
2020-11-23 12:05:26 +0100

    Commit #2
    Finished Opdracht A

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 // Stijn Dusseldorp
 // Software Developer
 
-const days = ['Maandag', 'Dinsdag','Woensdag','Donderdag','Vrijdag','Zaterdag','Zondag.'];
+const days = ['Maandag', 'Dinsdag','Woensdag','Donderdag','Vrijdag','Zaterdag','Zondag'];
 
 
+// De code voor heel de week
 for(i = 0; i < days.length; i++){
-    document.getElementById('week') .innerHTML += days[i] + " ";
+    document.getElementById('week').innerHTML += days[i] + " ";
+}
+
+// De code voor de werkdagen
+for(i = 0; i < 5; i++){
+    document.getElementById('werkdagen').innerHTML += days[i] + " ";
+}
+
+// De code voor het weekend
+for(i = 5; i < 7; i++){
+    document.getElementById('weekenddagen').innerHTML += days[i] + " ";
+}
+
+// De code voor de omgekeerde week
+for(i = 6; i >=0 ; i--){
+    document.getElementById('reverse').innerHTML += days[i] + " ";
+}
+
+// De code voor de omgekeerde werkweek
+for(i =4; i >= 0; i--){
+    document.getElementById('werkReverse').innerHTML += days[i] + " ";
+}
+
+//De code voor het omgekeerde weekend
+for(i = 6; i >= 5; i--){
+    document.getElementById('weekendReverse').innerHTML += days[i] + " ";
 }
