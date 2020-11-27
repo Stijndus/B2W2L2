05ea57abd2ef8c3f4ca936b04cba10f45d1ca031
Stijn <99053054@mydavinci.nl>
2020-11-27 10:14:15 +0100

    Commit #4
    Removed dry code in Opdracht C

--- Opdracht C/script.js
+++ Opdracht C/script.js
@@ -4,22 +4,9 @@
 var tables = [2,4,6,8]
 var container = document.getElementById("outputContainer")
 
-for(i=1;i<=10;i++){
-    let answer = tables[0] * i;
-    container.innerHTML += tables[0] + " x " + i + " = " + answer + "<br>"
-}
-
-for(i=1;i<=10;i++){
-    let answer = tables[1] * i;
-    container.innerHTML += tables[1] + " x " + i + " = " + answer + "<br>"
-}
-
-for(i=1;i<=10;i++){
-    let answer = tables[2] * i;
-    container.innerHTML += tables[2] + " x " + i + " = " + answer + "<br>"
-}
-
-for(i=1;i<=10;i++){
-    let answer = tables[3] * i;
-    container.innerHTML += tables[3] + " x " + i + " = " + answer + "<br>"
+for(x=0;x<tables.length;x++){
+    for(i=1;i<=10;i++){
+        let answer = tables[x] * i;
+        container.innerHTML += tables[x] + " x " + i + " = " + answer + "<br>"
+    }
 }
