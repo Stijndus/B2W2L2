b9a623c191cd90821d88c7fb45cfa8adbc53d3c6
Stijn <99053054@mydavinci.nl>
2020-11-27 11:04:14 +0100

    Commit #7
    Finished Opdracht D

---
+++ Opdracht D/script.js
@@ -0,0 +1,6 @@
+// Stijn Dusseldorp
+// Software Developer 2020
+
+var games = ['Monopoly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo'];
+
+document.write(games[Math.floor((Math.random() * games.length))]);
