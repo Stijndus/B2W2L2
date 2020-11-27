dfcb74fdf46be39dfd229d5148a69ac72b5c0623
Stijn <99053054@mydavinci.nl>
2020-11-27 10:38:05 +0100

    Commit #5

--- Opdracht C/script.js
+++ Opdracht C/script.js
@@ -2,11 +2,14 @@
 // Software Developer 2020
 
 var tables = [2,4,6,8]
-var container = document.getElementById("outputContainer")
+var container = document.getElementById('outputContainer')
 
 for(x=0;x<tables.length;x++){
+    var tableHead = document.createElement('h2');
+    tableHead.innerText = ('Dit is de kop van de tafel ' + tables[x]);
+    container.appendChild(tableHead);
     for(i=1;i<=10;i++){
         let answer = tables[x] * i;
-        container.innerHTML += tables[x] + " x " + i + " = " + answer + "<br>"
+        container.innerHTML += tables[x] + ' x ' + i + ' = ' + answer + '<br>'
     }
 }
